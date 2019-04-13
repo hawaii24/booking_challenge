c9707327fd18d8bad240dd04d6e7217b0856d0b7
Biko Pougala <biko.pougala@outlook.com>
2019-04-14 00:04:46 +0800

    Part 1 started

--- src/apis/PartOne.java
+++ src/apis/PartOne.java
@@ -1,8 +1,61 @@
 package apis;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class PartOne {
 
+    private static String getstuff(String pickup, String dropoff) {
+
+        String result = "";
+        try {
+            String url_stringify = String.format("https://techtest.rideways.com/dave?pickup=%s&dropoff=%s", pickup, dropoff);
+            URL url = new URL(url_stringify);
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int status = con.getResponseCode();
+
+            if(status == 200) { // success, read the output
+                BufferedReader bf = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer content = new StringBuffer();
+                while((inputLine = bf.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                bf.close();
+                con.disconnect();
+
+            } else {
+                System.out.println("Sorry, could not access Dave's API");
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException p) {
+            p.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private static String[] organiseOutput(String output) {
+        String[] result = new String[];
+
+        return result;
+    }
+
     public static void main(String args[]) {
+        String pickup = args[0];
+        String dropoff = args[1];
+        getstuff(pickup, dropoff);
         System.out.println("Hello, world!");
     }
 }
