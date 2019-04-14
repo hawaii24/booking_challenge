e5ac889ef2386832a8a448ffe2dabb3adbd9f9f0
Biko Pougala <biko.pougala@outlook.com>
2019-04-15 01:16:03 +0800

    Parses JSON output obtained from GET request to Dave's API

---
+++ src/apis/OutputParser.java
@@ -0,0 +1,23 @@
+package apis;
+
+class OutputParser {
+
+    private static String[] listRanker(StringBuffer output) {
+        // this method takes the output of the API call and ranks the results by distance
+        String[] response = new String[];
+
+
+
+        return response;
+    }
+
+    private String bufferParser(StringBuffer charSeq) {
+
+        for(int i = 0;  i < charSeq.length(); i++) {
+            char currentCh = charSeq.charAt(i);
+        }
+
+        return "";
+    }
+
+}
