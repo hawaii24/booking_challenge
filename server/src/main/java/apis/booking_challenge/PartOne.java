1ba433d967ccfd0c0daabb497796bb10770ee03b
Biko Pougala <bpougala@gmail.com>
2019-04-22 22:55:40 +0200

    Update PartOne.java

--- server/src/main/java/apis/booking_challenge/PartOne.java
+++ server/src/main/java/apis/booking_challenge/PartOne.java
@@ -24,7 +24,6 @@ public class PartOne {
 	}
 
     public Map<String, Integer> getstuff(String api, int num_pass) {
-    	//List<String> resultStrings = new ArrayList<String>(); 
     	if(num_pass < 1 || num_pass > 16) {
     		Map<String, Integer> error_message = new HashMap<String, Integer>(); 
     		error_message.put("The number of passangers must be between 1 and 16", 0);
@@ -35,21 +34,19 @@ public class PartOne {
             URL url = new URL(url_stringify);
             HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            //con.setConnectTimeout(3000);
+            con.setConnectTimeout(2000);
             int status = con.getResponseCode();
             if(status == 200) { // success, read the output
                 InputStreamReader stmread = new InputStreamReader(con.getInputStream());
                 Map<String, Integer> results = OutputParser.listAllResults(stmread, num_pass);
-                
-                System.out.println(results.toString());
-                con.disconnect();
+		con.disconnect();
                 
                 
                 return results; 
 
             } else {
             	String errorMessage = String.format("Sorry, could not access %s's API", api);
-                System.out.println(errorMessage);
+                System.out.println(con.getErrorStream());
                 Map<String, Integer> error = new HashMap<String, Integer>(); 
                 error.put(errorMessage, 0);
                 
@@ -81,6 +78,7 @@ public class PartOne {
 		} else if(!acceptableApis.contains(args[2])) {
 			System.out.println("ERROR: Please enter as API one of: dave, eric, jeff."); 
 		} else {
+			// make sure arguments are genuine GPS lat-long coordinates 
 			String coordinateChecker = "^(\\-?\\d+(\\.\\d+)?),\\s*(\\-?\\d+(\\.\\d+)?)$"; 
 			if(!(args[0].matches(coordinateChecker) && args[1].matches(coordinateChecker))) {
 				System.out.println("ERROR: Please enter valid coordinates as input"); 
@@ -90,8 +88,7 @@ public class PartOne {
 					int num_pass = Integer.parseInt(args[2]); 
 					Map<String, Integer> results = exercise.getstuff("dave", num_pass);
 					for(Map.Entry<String, Integer> entry: results.entrySet()) {
-						String tobePrinted = String.format("%s - %d", entry.getKey(), entry.getValue());
-						System.out.println(tobePrinted); 
+						System.out.println(String.format("%s - %d", entry.getKey(), entry.getValue())); 
 					}
 
 				} catch (NumberFormatException pe) {
